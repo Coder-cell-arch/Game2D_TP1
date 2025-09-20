bbff6b752572e9b5e8464afa7301d1ec54c51dd3
Ezequiel <ezemerani3@gmail.com>
2025-09-20 13:34:19 -0300

    Bullet: remove Door checkl (only Ground; dummy deletes on hitt)

--- Assets/Scripts/Bullet.cs
+++ Assets/Scripts/Bullet.cs
@@ -1,29 +1,27 @@
 using UnityEngine;
 
 public class Bullet : MonoBehaviour
-
 {
-    public float speed = 10f, lifetime = 3f;
+    public float speed = 10f;
+    public float lifetime = 3f;
 
-
-    private void Start() { 
-
-    Destroy(gameObject, lifetime); 
-
-    }
-
-
-    private void Update() { 
-
-    transform.Translate(Vector2.right * speed * Time.deltaTime); 
-    
-    }
-
-
-
-    private void OnTriggerEnter2D(Collider2D other)
+    void Start()
     {
-        if (other.CompareTag("Ground") || other.CompareTag("Door")) Destroy(gameObject);
+        Destroy(gameObject, lifetime);
+    }
+
+    void Update()
+    {
+        transform.Translate(Vector2.right * speed * Time.deltaTime);
+    }
+
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.CompareTag("Ground"))
+        {
+            Destroy(gameObject);
+        }
     }
 }
+
 
